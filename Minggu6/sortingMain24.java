220f0c91539c019258536f5bafc16fb144b27d41
RayaRabbani129 <arya.rabbani06@gmail.com>
2025-03-18 08:58:19 +0700

    halo

--- Minggu6/sortingMain24.java
+++ Minggu6/sortingMain24.java
@@ -1,4 +1,12 @@
 public class sortingMain24 {
-    int a[] = {20, 10, 2, 7, 12};
-    
+    public static void main(String[] args) {
+        int a[] = {20, 10, 2, 7, 12};
+        sorting24 darurat1 = new sorting24(a, a.length);
+
+        System.out.println("Data awal 1");
+        darurat1.tampil();
+        darurat1.bubbleSort();
+        System.out.println("Data sudah diurutkan dengan BUBBLE SORT (ASC)");
+        darurat1.tampil();
+    }
 }
