3d5b621e51e7097ac6c62acb583517c3a262ff63
RayaRabbani129 <arya.rabbani06@gmail.com>
2025-03-18 09:30:19 +0700

    aku

--- Minggu6/sortingMain24.java
+++ Minggu6/sortingMain24.java
@@ -2,8 +2,10 @@ public class sortingMain24 {
     public static void main(String[] args) {
         int a[] = {20, 10, 2, 7, 12};
         int b[] = {30, 20, 2, 8, 14};
+        int c[] = {40, 10, 4, 9, 3};
         sorting24 darurat1 = new sorting24(a, a.length);
         sorting24 darurat2 = new sorting24(b, b.length);
+        sorting24 darurat3 = new sorting24(c, c.length);
 
         System.out.println("Data awal 1");
         darurat1.tampil();
@@ -15,5 +17,10 @@ public class sortingMain24 {
         darurat2.selectionSort();
         System.out.println("Data sudah diurutkan dengan SELECTION SOR (ASC)");
         darurat2.tampil();
+        System.out.println("Data awal 3");
+        darurat3.tampil();
+        darurat3.bubbleSort();
+        System.out.println("Data sudah diurutkan dengan INSERTION SORT (ASC)");
+        darurat3.tampil();
     }
 }
